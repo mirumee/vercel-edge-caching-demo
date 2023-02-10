59c68fc64d549aca2403b920d6a45fdcafc4cc9c
Mykhailo Bloshko <mykhailo.bloshko@mirumee.com>
2023-02-10 22:46:05 +0100

    Add max-age

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -7,11 +7,12 @@ import {
 const login: NextApiHandler = (req, res) => {
   const isDevelopment = process.env.NODE_ENV === "development";
 
-  const mockAuthorizationCookie = `${MOCK_AUTHORIZATION_COOKIE_NAME}=1; path=/; ${
-    isDevelopment ? "" : "Secure;"
-  } SameSite=Strict;`;
+  const maxAge = "Max-Age=3600;";
+  const secure = isDevelopment ? "" : "Secure;";
 
-  const vercelNoCacheCookie = `${VERCEL_NO_CACHE_COOKIE_NAME}=1; path=/; SameSite=Strict;`;
+  const mockAuthorizationCookie = `${MOCK_AUTHORIZATION_COOKIE_NAME}=1; path=/; ${secure} ${maxAge} SameSite=Strict;`;
+
+  const vercelNoCacheCookie = `${VERCEL_NO_CACHE_COOKIE_NAME}=1; path=/; ${secure} ${maxAge} SameSite=Strict;`;
 
   res
     .setHeader("Set-Cookie", [mockAuthorizationCookie, vercelNoCacheCookie])
